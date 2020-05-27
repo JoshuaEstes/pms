681276b919a8597e589a0cbb643991a36d4cd894
Joshua Estes <joshuaestes@users.noreply.github.com>
2020-05-27 01:08:23 +0000

    meh

--- scripts/pms-upgrade.sh
+++ scripts/pms-upgrade.sh
@@ -16,36 +16,32 @@ _update_pms_files() {
   cp -v $PMS/templates/bashrc ~/.bashrc
 }
 
-main() {
-  if [ "$PMS_DEBUG" -eq "1" ]; then
-    echo
-    echo "-=[ PMS Debug ]=-"
-    echo "PMS:         $PMS"
-    echo "PMS_DEBUG:   $PMS_DEBUG"
-    echo "PMS_REPO:    $PMS_REPO"
-    echo "PMS_REMOTE:  $PMS_REMOTE"
-    echo "PMS_BRANCH:  $PMS_BRANCH"
-    echo "PMS_THEME:   $PMS_THEME"
-    echo "PMS_PLUGINS: $PMS_PLUGINS"
-    echo "PMS_SHELL:   $PMS_SHELL"
-    echo "-=[ PMS Debug ]=-"
-    echo
-  fi
-  cwd_checkpoint="$CWD"
-  cd "$PMS"
+if [ "$PMS_DEBUG" -eq "1" ]; then
   echo
-  echo "Upgrading to latest PMS version"
+  echo "-=[ PMS Debug ]=-"
+  echo "PMS:         $PMS"
+  echo "PMS_DEBUG:   $PMS_DEBUG"
+  echo "PMS_REPO:    $PMS_REPO"
+  echo "PMS_REMOTE:  $PMS_REMOTE"
+  echo "PMS_BRANCH:  $PMS_BRANCH"
+  echo "PMS_THEME:   $PMS_THEME"
+  echo "PMS_PLUGINS: $PMS_PLUGINS"
+  echo "PMS_SHELL:   $PMS_SHELL"
+  echo "-=[ PMS Debug ]=-"
   echo
-  git pull origin master
-  echo
-  echo "Copying files"
-  echo
-  _update_pms_files
-  echo
-  echo "Upgrade complete"
-  echo
-  exec $PMS_SHELL
-  cd "$cwd_checkpoint"
-}
-
-main
+fi
+cwd_checkpoint="$CWD"
+cd "$PMS"
+echo
+echo "Upgrading to latest PMS version"
+echo
+git pull origin master
+echo
+echo "Copying files"
+echo
+_update_pms_files
+echo
+echo "Upgrade complete"
+echo
+exec $PMS_SHELL
+cd "$cwd_checkpoint"
