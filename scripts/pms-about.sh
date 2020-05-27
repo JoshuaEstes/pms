3919cfab73ae01ecbf9a0697ea284d393a3cbfe4
Joshua Estes <joshuaestes@users.noreply.github.com>
2020-05-27 01:01:40 +0000

    update about

--- scripts/pms-about.sh
+++ scripts/pms-about.sh
@@ -1,13 +1,2 @@
-#!/usr/bin/env bash
-set -e
-PMS=${PMS:-~/.pms}
-
-if [ "$PMS_DEBUG" -eq "1" ]; then
-  echo "DEBUG MODE HAS BEEN ENABLED"
-fi
-
-echo "PMS:       $PMS"
-echo "PMS_LOCAL: $PMS_LOCAL"
-echo "PMS_THEME: $PMS_THEME"
-
-return
+# Just do a diagonstic dump
+_pms_diagonstic_dump
