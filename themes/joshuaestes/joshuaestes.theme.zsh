834c8eb379a8ce7633a6421ac6b00bf40b5c523c
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2020-05-27 08:48:19 -0400

    adding theme for myself to use

---
+++ themes/joshuaestes/joshuaestes.theme.zsh
@@ -0,0 +1,4 @@
+PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M %F{green}[%F{blue}$(phpbrew_current_php_version)%F{green}][%F{cyan}${vcs_info_msg_0_}%F{green}]%f%k
+%F{green}%~%f%k %F{grey}%#%f%k '
+RPS1='%f%k'
+RPS2='%f%k'
