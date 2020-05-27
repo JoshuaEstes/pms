8b2be20e02155b31d43b0677dd5a7fc0baf8a916
Joshua Estes <joshuaestes@users.noreply.github.com>
2020-05-27 06:49:09 +0000

    adding more in there

---
+++ plugins/pms/pms-theme-list.sh
@@ -0,0 +1,14 @@
+# pms theme list
+echo
+echo "Core Themes:"
+for theme in $PMS/themes/; do
+  echo "$theme"
+done
+echo
+echo "Local Themes:"
+for theme in $PMS_LOCAL/themes/; do
+  echo "$theme"
+done
+echo
+echo "Current Theme: $PMS_THEME"
+echo
