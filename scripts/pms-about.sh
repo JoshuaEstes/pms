73cd621196b7617a88b59b3f5c8abd88fc25f3c7
Joshua Estes <joshuaestes@users.noreply.github.com>
2020-05-27 05:41:21 +0000

    thats embarassing

--- scripts/pms-about.sh
+++ scripts/pms-about.sh
@@ -1,2 +1,9 @@
-# Just do a diagonstic dump
-_pms_diagonstic_dump
+# pms about
+echo
+echo "PMS Manager"
+echo "Making you more productive in your shell than a turtle"
+echo
+echo "Source:         https://github.com/JoshuaEstes/pms"
+echo "User Docs:      https://joshuaestes.github.io/pms/"
+echo "Developer Docs: https://github.com/JoshuaEstes/pms/wiki"
+echo
