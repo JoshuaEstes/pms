7eb3b26e1fd93f19a188de254ade2c0c17a4ee9a
Joshua Estes <joshuaestes@users.noreply.github.com>
2020-05-27 06:53:08 +0000

    bug fix

--- plugins/pms/pms-theme-list.sh
+++ plugins/pms/pms-theme-list.sh
@@ -1,13 +1,15 @@
 # pms theme list
 echo
 echo "Core Themes:"
-for theme in $PMS/themes/; do
-  echo "$theme"
+for theme in $PMS/themes/*; do
+  theme=${theme%*/}
+  echo "${theme##*/}"
 done
 echo
 echo "Local Themes:"
-for theme in $PMS_LOCAL/themes/; do
-  echo "$theme"
+for theme in $PMS_LOCAL/themes/*; do
+  theme=${theme%*/}
+  echo "${theme##*/}"
 done
 echo
 echo "Current Theme: $PMS_THEME"
