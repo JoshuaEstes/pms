2e851d0af3245f74ca9eba86a327ed0ac82cad43
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2020-05-27 22:20:34 -0400

    meh

--- themes/joshuaestes/joshuaestes.theme.zsh
+++ themes/joshuaestes/joshuaestes.theme.zsh
@@ -1,6 +1,6 @@
 # joshua@joshua.local [PHP Info] [Git Information]
 # ~/code/joshuaestes/pms
-PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M %f%k
+PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M%F{green}[%F{blue}$(phpbrew_current_php_version)%F{green}][%F{cyan}${vcs_info_msg_0_}%F{green}]%f%k
 %F{green}%~%f%k %F{grey}%#%f%k '
-#RPS1='%f%k'
-#RPS2='%f%k'
+RPS1='%f%k'
+RPS2='%f%k'
