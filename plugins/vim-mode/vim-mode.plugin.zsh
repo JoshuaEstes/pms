117ed3569d4182da53de53833054921e9cb60ec8
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2020-05-27 22:14:00 -0400

    getting closer to what it was

---
+++ plugins/vim-mode/vim-mode.plugin.zsh
@@ -0,0 +1,12 @@
+bindkey -v # viins
+function zle-line-init zle-keymap-select {
+	case $KEYMAP in
+		(main|viins) RPS1='%F{cyan%}INSERT%{$reset_color%}';;
+		(vicmd) RPS1='%F{red%}NORMAL%{$reset_color%}';;
+		(*) RPS1='%F{white%}%K{red}${KEYMAP}%{$reset_color%}';;
+	esac
+	RPS2='%{$reset_color%}'
+	zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
--- themes/joshuaestes/joshuaestes.theme.zsh
+++ themes/joshuaestes/joshuaestes.theme.zsh
@@ -1,4 +1,6 @@
+# joshua@joshua.local [PHP Info] [Git Information]
+# ~/code/joshuaestes/pms
 PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M %f%k
 %F{green}%~%f%k %F{grey}%#%f%k '
-RPS1='%f%k'
-RPS2='%f%k'
+#RPS1='%f%k'
+#RPS2='%f%k'
