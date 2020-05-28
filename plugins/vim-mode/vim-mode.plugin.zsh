7ac459b5120b067a3141bf9a9850bdf87638fea3
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2020-05-27 22:24:45 -0400

    updates

--- plugins/vim-mode/vim-mode.plugin.zsh
+++ plugins/vim-mode/vim-mode.plugin.zsh
@@ -1,11 +1,11 @@
 bindkey -v # viins
 function zle-line-init zle-keymap-select {
 	case $KEYMAP in
-		(main|viins) RPS1='%F{cyan%}INSERT%{$reset_color%}';;
-		(vicmd) RPS1='%F{red%}NORMAL%{$reset_color%}';;
-		(*) RPS1='%F{white%}%K{red}${KEYMAP}%{$reset_color%}';;
+		(main|viins) RPS1="%F{cyan%}INSERT%{$reset_color%}";;
+		(vicmd) RPS1="%F{red%}NORMAL%{$reset_color%}";;
+		(*) RPS1="%F{white%}%K{red}${KEYMAP}%{$reset_color%}";;
 	esac
-	RPS2='%{$reset_color%}'
+	RPS2="%{$reset_color%}"
 	zle reset-prompt
 }
 zle -N zle-line-init
