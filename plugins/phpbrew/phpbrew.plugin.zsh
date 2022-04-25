d98f3d9a6872b0eff4b5d7c829d495b1245e689f
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2022-04-25 08:46:14 -0400

    meh

--- plugins/phpbrew/phpbrew.plugin.zsh
+++ plugins/phpbrew/phpbrew.plugin.zsh
@@ -1,1 +1,14 @@
 [[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
+
+function pms_phpbrew_current_php_version() {
+  if type "php" > /dev/null; then
+    local version=$(php -v | grep -E "PHP [578]" | sed 's/.*PHP \([^-]*\).*/\1/' | cut -c 1-6)
+    if [[ -z "$PHPBREW_PHP" ]]; then
+      echo "php:$version-system"
+    else
+      echo "php:$version-phpbrew"
+    fi
+  else
+     echo "php:not-installed"
+  fi
+}
--- themes/joshuaestes/joshuaestes.theme.zsh
+++ themes/joshuaestes/joshuaestes.theme.zsh
@@ -1,7 +1,7 @@
 # joshua@joshua.local [PHP Info] [Git Information]
 # ~/code/joshuaestes/pms
 setopt prompt_subst
-PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M%F{green}[%F{blue}$(phpbrew_current_php_version)%F{green}][%F{cyan}${vcs_info_msg_0_}%F{green}]%f%k
+PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M%F{green}[%F{blue}$(pms_phpbrew_current_php_version)%F{green}][%F{cyan}${vcs_info_msg_0_}%F{green}]%f%k
 %F{green}%~%f%k %F{grey}%#%f%k '
 RPS1="%f%k"
 RPS2="%f%k"
