8cbd012a730ca39a74b602a34b19a2eada035123
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2024-10-23 11:22:19 -0400

    meh

---
+++ plugins/homebrew/env
@@ -0,0 +1,1 @@
+export PATH="/opt/homebrew/sbin:/opt/homebrew/bin:$PATH"
--- themes/joshuaestes/joshuaestes.theme.zsh
+++ themes/joshuaestes/joshuaestes.theme.zsh
@@ -1,7 +1,15 @@
 # joshua@joshua.local [PHP Info] [Git Information]
 # ~/code/joshuaestes/pms
 setopt prompt_subst
-PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M%F{green}[%F{blue}$(pms_phpbrew_current_php_version)%F{green}][%F{cyan}${vcs_info_msg_0_}%F{green}]%f%k
+function _php_version() {
+  if type "php" > /dev/null; then
+    local version=$(php -v | grep -E "PHP [578]" | sed 's/.*PHP \([^-]*\).*/\1/' | cut -c 1-6)
+    echo "php:$version"
+  else
+     echo "php:not-installed"
+  fi
+}
+PS1='%f%k%F{magenta}%n%F{grey}@%F{yellow}%M%F{green}[%F{blue}$(_php_version)%F{green}][%F{cyan}${vcs_info_msg_0_}%F{green}]%f%k
 %F{green}%~%f%k %F{grey}%#%f%k '
 RPS1="%f%k"
 RPS2="%f%k"
