9db1e65ff62b517d8980712b548dc19f17f04396
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2024-10-27 20:18:01 -0400

    meh

---
+++ lib/logger.sh
@@ -0,0 +1,37 @@
+####
+#
+# Allows various log messages
+#
+####
+
+_logger_emergency() {
+    printf "\r$1\n"
+}
+
+_logger_alert() {
+    printf "\r$1\n"
+}
+
+_logger_critical() {
+    printf "\r$1\n"
+}
+
+_logger_error() {
+    printf "\r$1\n"
+}
+
+_logger_warning() {
+    printf "\r$1\n"
+}
+
+_logger_notice() {
+    printf "\r$1\n"
+}
+
+_logger_info() {
+    printf "\r$1\n"
+}
+
+_logger_debug() {
+    printf "\r$1\n"
+}
